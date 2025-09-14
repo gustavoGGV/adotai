3e0b3b8f9badbd9697242c8a394550488567582a
gustavoGGV <gustavo.vieira.tds2023@gmail.com>
2025-09-14 17:30:06 -0300

    feat: finish login page

---
+++ view/cadastro.php
@@ -0,0 +1,8 @@
+<?php
+include_once(__DIR__ . "/componentes/configuracao-da-pagina.php");
+?>
+
+<title>Adotaí | Cadastrar-se</title>
+</head>
+
+<p>Teste</p>
--- view/componentes/footer.php
+++ view/componentes/footer.php
@@ -1,4 +1,4 @@
-<footer class="text-muted mt-3">
+<footer class="text-muted mt-5">
   <div class="text-center p-3">
     © 2025 Copyright:
     <a href="https://github.com/gustavoGGV/" target="_blank"><i class="bi bi-github"></i> gustavoGGV</a>
