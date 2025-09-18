30210482340add226a00f5229d78fd0d0e4c87a2
gustavoGGV <gustavo.vieira.tds2023@gmail.com>
2025-09-18 09:27:54 -0300

    feat: criar navbar; ajustar footer

---
+++ view/perfil.php
@@ -0,0 +1,14 @@
+<?php
+include_once(__DIR__ . "/acoes/adquirir-informacao-do-usuario.php");
+include_once(__DIR__ . "/componentes/configuracao-da-pagina.php");
+?>
+
+<title>Adotaí | Perfil</title>
+</head>
+
+<?php
+include_once(__DIR__ . "/componentes/navbar.php");
+?>
+
+<?php
+include_once(__DIR__ . "/componentes/footer.php");
