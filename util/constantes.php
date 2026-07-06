48b72a83ca3b774c0b5d40c52ae84c09012805e7
gustavoggv <gustavo.vieira.tds2023@gmail.com>
2026-07-06 15:37:06 -0300

    fix: verificar se precisa do arquivo .env em constantes

--- util/constantes.php
+++ util/constantes.php
@@ -2,8 +2,10 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-$dotenv->load();
+if (file_exists(__DIR__ . "/../.env")) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+    $dotenv->load();
+}
 
 // Configurar essas variáveis de acordo com o seu ambiente
 define("DB_HOST", $_ENV["DB_HOST"]);
