64501d36a6216993d27b265c624c7ab41f4d441c
gustavoggv <gustavo.vieira.tds2023@gmail.com>
2026-07-08 13:30:10 -0300

    build: atualizar config dos cookies

--- view/acoes/deslogar.php
+++ view/acoes/deslogar.php
@@ -3,17 +3,24 @@
 require_once __DIR__ . "/../../util/constantes.php";
 
 if ($_COOKIE) {
-    // Para deletar um cookie, é necessário passar todas as mesmas informações, menos o valor e o tempo de expiramento que é revertido.
-    setcookie("idUsu", "", time() - 60 * 60 * 24 * 120, "/", "", false, true);
-    setcookie(
-        "telefoneUsu",
-        "",
-        time() - 60 * 60 * 24 * 120,
-        "/",
-        "",
-        false,
-        true,
-    );
+  // Para deletar um cookie, é necessário passar todas as mesmas informações, menos o valor e o tempo de expiramento que é revertido.
+  setcookie("idUsu", "", [
+    "expires" => time() - 60 * 60 * 24 * 120,
+    "path" => "/",
+    "domain" => "",
+    "secure" => true,
+    "httponly" => true,
+    "samesite" => "None",
+  ]);
+
+  setcookie("telefoneUsu", "", [
+    "expires" => time() - 60 * 60 * 24 * 120,
+    "path" => "/",
+    "domain" => "",
+    "secure" => true,
+    "httponly" => true,
+    "samesite" => "None",
+  ]);
 }
 
 header("location: " . URL_BASE . "/view/login.php");
